1c9ce081632ff2397fc426c81ff16b07d91e8f0e
aaliyasiddiqua <144471417+aaliyasiddiqua@users.noreply.github.com>
2023-09-08 10:10:13 -0700

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,16 @@
+
+
+def leap_yr(n):
+
+  if((n%4==0 and n%100!=0 or n%400==0)):
+  
+ print("{} is a leap year".format(n))
+ 
+ else:
+   
+ print("{} is not aleap year".format
+
+
+n=int(input("Enter a year:"))
+
+yr=leap_yr(n)
